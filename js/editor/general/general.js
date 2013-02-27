00f5223a8c94a9b00695e48d0700e103090e5638
sjpsega <ssjjpp520@126.com>
2013-02-27 21:20:48 +0800

    ci

--- js/editor/general/general.js
+++ js/editor/general/general.js
@@ -22,7 +22,7 @@ var GeneralView = ChartView.extend({
         this.views.push(legendView);
         this.model.push(legendView.model);
 
-        var tooltipView = new ToooltipView({
+        var tooltipView = new TooltipView({
             modelClz:TooltipModel,
             modelAttributes : StyleCenter.getInstance().getStyle("tooltip"),
             el:$("#tooltip-set")
